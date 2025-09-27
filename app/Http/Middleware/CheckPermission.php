a593ebd76e9b4c75b53be4d4971235fc0322ebe9
Ahmed Kalalsh <your.email@example.com>
2025-09-27 14:18:42 +0300

    feat: add CheckPermission middleware

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next 
+     * @param string $permission
+     */
+    public function handle(Request $request, Closure $next, string $permission): Response
+    {
+        $user = $request->user();
+
+        if (!$user || !$user->hasPermission($permission)) {
+            throw new AuthorizationException('Forbidden');
+        }
+        return $next($request);
+    }
+}
